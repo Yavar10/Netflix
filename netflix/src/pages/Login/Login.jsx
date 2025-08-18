9e221b717c6b440a981d8eb8667bf8382b9c1855
Yavar <zuri00014@gmail.com>
2025-08-18 10:57:57 +0530

    completed frontend of login page

--- netflix/src/pages/Login/Login.jsx
+++ netflix/src/pages/Login/Login.jsx
@@ -3,25 +3,40 @@ import "./Login.css"
 import logo from '../../assets/assets/logo.png'
 const Login = () => {
   return (
-    <div>
+    <div className='box'>
       <img src={logo} className='login-logo' alt="" />
+      <div className="content">
       <div className='login-form'>
+        <form className='form'>
+        <div className='signin'>
         <h1>
           Sign In
         </h1>
-        <form>
-          <input type="text" placeholder='Your name' />
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
-          <button>Sign In</button>
+        </div>
+        
+        {/*   <input className='log-input' type="text" placeholder='Your name' /> */}
+          
+          
+        
+          <input className='log-input' type="email" placeholder='Email' />
+       
+          
+          
+          <input className='log-input' type="password" placeholder='Password' />
+     
+  
+          <button className='signin-btn'>Sign In</button>
+      
+
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
               <label htmlFor="">Remember me</label>
             </div>
-            <p>Need help</p>
+            <p className='nh'>Need help?</p>
           </div>
         </form>
+      </div>
       </div>
     </div>
   )
