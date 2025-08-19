b4b5a12a039e2cdb5081d002496da099adf4856c
Yavar <zuri00014@gmail.com>
2025-08-20 00:13:18 +0530

    player trailer push

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
 import './App.css'
+
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
+
 import { Routes,Route } from 'react-router-dom'
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/'element={<Home/>} />
         <Route path='/login'element={<Login/>} />
+        <Route path='/player'element={<Player/>} />
       </Routes>
     </div>
   )
